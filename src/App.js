dbdf7feb68b75f929f6d03f495631e122245945b
Carlos De La Cantera <carlosecantera@yahoo.com>
2021-05-25 19:29:58 -0700

    edits

--- src/App.js
+++ src/App.js
@@ -2,35 +2,49 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component {
+
   constructor() {
     super();
+    this.textInput=React.createRef();
+
     this.state = {
-      data: {},
+      location: null,
+      map: null,
+      searchQuery: '',
+    };
+  }
+
+    setQuery = (e) => {
+      e.preventDefault();
+      // const query = e.target.value;
+      // console.log('This is the query', query);
+      // this.setState({ searchQuery: query });
+      this.handleLocation(this.textInput.current.value);
     }
-  }
 
-  fetchData = () => {
-    axios.get('https://us1.locationiq.com/v1/search.php')
-    .then(response => console.log(response))
-    .catch(error => console.error(error));
-  }
+    handleLocation = async (searchQuery) => {
+      console.log('City we search for', searchQuery);
+      let locationResponseData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`);
+      console.log(locationResponseData);
+      this.setState( { location: locationResponseData.data[0] });
+    }
 
-  fetchDataCityExplorer = CityExplorer () => {
-    let response = await axios.get('https://us1.locationiq.com/v1/search.php');
-    console.log(response);
-  }
+    render() {
+      return (
+        <div className="App">
+          <h1>Search City</h1>
+          <form onSubmit={this.setQuery}>
 
-
-  render(); {
-    return (
-      <div className="App">
-        <button onClick={this.fetchData}>Get Data</button>
-      </div>
-    );
-  }
+            <input type="text" ref={this.textInput} />
+            <button type="submit">Get Data</button>
+          </form>
+          {this.state.location ? this.state.location.display_name : 'Location'}
+        </div>
+      );
+    }
 }
 
 
